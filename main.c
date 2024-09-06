11cfeabe58e6840a1d70c9dc904c8a9834f2055c
MatiasDevv <gaunam134@gmail.com>
2024-09-06 14:58:11 -0300

    separando responsabilidades

--- main.c
+++ main.c
@@ -1,8 +1,96 @@
-#include <stdio.h>
 #include <SDL2/SDL.h>
+#include <stdio.h>
+
+// Velocidad de la serpiente
+int snake_velX = 20;
+int snake_velY = 0;
+
+// Bucle del juego
+int running = 1;
+SDL_Event event;
+
+// Variables del juego (por simplicidad, sólo una cabeza de serpiente y una comida)
+SDL_Rect snake = {100, 100, 20, 20}; // Posición y tamaño de la serpiente
+SDL_Rect food = {200, 200, 20, 20};  // Posición y tamaño de la comida
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
+
+int snakeMovement(SDL_Event event)
+{
+    int w = snake.w;
+    int h = snake.h;
+    switch (event.key.keysym.sym)
+    {
+    case SDLK_UP:
+        snake_velX = 0;
+        snake_velY = -20;
+        if (snake.w != 20)
+        {
+            snake.h = snake.w;
+            snake.w = h;
+        }
+        break;
+    case SDLK_DOWN:
+        snake_velX = 0;
+        snake_velY = 20;
+        if (snake.w != 20)
+        {
+            snake.h = snake.w;
+            snake.w = h;
+        }
+        break;
+    case SDLK_LEFT:
+        snake_velX = -20;
+        snake_velY = 0;
+        if (snake.h != 20)
+        {
+            snake.w = snake.h;
+            snake.h = w;
+        };
+        break;
+    case SDLK_RIGHT:
+        snake_velX = 20;
+        snake_velY = 0;
+        if (snake.h != 20)
+        {
+            snake.w = snake.h;
+            snake.h = w;
+        };
+        break;
+    case SDLK_ESCAPE:
+        running = 0;
+    }
+}
+
+void drawScreen(SDL_Renderer *renderer)
+{
+    // Limpia la pantalla
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Negro
+    SDL_RenderClear(renderer);
+
+    // Dibuja la comida
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Rojo
+    SDL_RenderFillRect(renderer, &food);
+
+    // Dibuja la serpiente
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Verde
+    SDL_RenderFillRect(renderer, &snake);
+
+    // Actualiza la pantalla
+    SDL_RenderPresent(renderer);
+
+    // Control de la velocidad del juego
+    SDL_Delay(150);
+}
+
+void cleanUp(SDL_Renderer *renderer, SDL_Window *window)
+{
+    // Limpieza
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,11 +102,12 @@ int main(int argc, char *argv[])
     }
 
     // Crea la ventana
-    SDL_Window *window = SDL_CreateWindow("Snake Game",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SCREEN_WIDTH, SCREEN_HEIGHT,
-                                          SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow(
+        "Snake Game",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN);
 
     if (!window)
     {
@@ -39,15 +128,6 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Variables del juego (por simplicidad, sólo una cabeza de serpiente y una comida)
-    SDL_Rect snake = {100, 100, 20, 20}; // Posición y tamaño de la serpiente
-    SDL_Rect food = {200, 200, 20, 20};  // Posición y tamaño de la comida
-    int velX = 20, velY = 0;             // Velocidad de la serpiente
-
-    // Bucle del juego
-    int running = 1;
-    SDL_Event event;
-
     while (running)
     {
         // Manejo de eventos
@@ -59,57 +139,24 @@ int main(int argc, char *argv[])
             }
             if (event.type == SDL_KEYDOWN)
             {
-                switch (event.key.keysym.sym)
-                {
-                case SDLK_UP:
-                    velX = 0;
-                    velY = -20;
-                    break;
-                case SDLK_DOWN:
-                    velX = 0;
-                    velY = 20;
-                    break;
-                case SDLK_LEFT:
-                    velX = -20;
-                    velY = 0;
-                    break;
-                case SDLK_RIGHT:
-                    velX = 20;
-                    velY = 0;
-                    break;
-                case SDLK_ESCAPE:
-                    running = 0;
-                }
+                snakeMovement(event);
             }
         }
 
+        if (SDL_HasIntersection(&snake, &food))
+        {
+            food.y = 10;
+            snake.h = snake.h + 20;
+        }
+
         // Movimiento de la serpiente
-        snake.x += velX;
-        snake.y += velY;
+        snake.x += snake_velX;
+        snake.y += snake_velY;
 
-        // Limpia la pantalla
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Negro
-        SDL_RenderClear(renderer);
-
-        // Dibuja la comida
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Rojo
-        SDL_RenderFillRect(renderer, &food);
-
-        // Dibuja la serpiente
-        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Verde
-        SDL_RenderFillRect(renderer, &snake);
-
-        // Actualiza la pantalla
-        SDL_RenderPresent(renderer);
-
-        // Control de la velocidad del juego
-        SDL_Delay(150);
+        drawScreen(renderer);
     }
 
-    // Limpieza
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    cleanUp(renderer, window);
 
     return 0;
 }
