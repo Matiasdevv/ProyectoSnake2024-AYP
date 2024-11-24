6e503e48fcc978c5e3e768617f0e2caeff907106
MatiasDevv <gaunam134@gmail.com>
2024-11-24 19:28:23 -0300

    agregando al .h de menu

--- ui/menu.h
+++ ui/menu.h
@@ -5,6 +5,10 @@
 #include <SDL2/SDL_ttf.h>
 
 void drawMenu(SDL_Renderer *renderer, GameState *gamestate);
-void handleMenuInput(SDL_Event event, GameState *gamestate,  SDL_Renderer *renderer,SDL_Window *window);
-void showMenu(SDL_Event event, GameState *gameState, SDL_Renderer *renderer,SDL_Window *window );
+void handleMenuInput(SDL_Event event, GameState *gamestate, SDL_Renderer *renderer, SDL_Window *window);
+void showMenu(SDL_Event event, GameState *gameState, SDL_Renderer *renderer, SDL_Window *window);
+void drawDifficultyMenu(SDL_Renderer *renderer, GameState *gameState);
+void handleDifficultyInput(SDL_Event event, GameState *gameState, SDL_Renderer *renderer, SDL_Window *window);
+void drawRanking(SDL_Renderer *renderer, GameState *gameState);
+void handleRankingInput(SDL_Event event, GameState *gameState, SDL_Renderer *renderer, SDL_Window *window);
 #endif
